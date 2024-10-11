2a051728d83895679bc70dcb8c714eb648a6beb1
Anthony Perez <aperez327@gmail.com>
2024-10-11 18:23:27 -0400

    Initial Commit

---
+++ YouAreAwesome8.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YouAreAwesome8.0
+//
+//  Created by Anthony Perez on 10/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouAreAwesome8.0/YouAreAwesome8_0App.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesome8_0App.swift
+//  YouAreAwesome8.0
+//
+//  Created by Anthony Perez on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesome8_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
